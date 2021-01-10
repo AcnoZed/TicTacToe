522a41c061b6f73df88816bb038cedcb76be1b98
AcnoZed <Me.ouss.am@outlook.de>
2021-01-10 16:29:32 +0100

    created Player

---
+++ src/main/java/TicTacToe/Player.java
@@ -0,0 +1,21 @@
+package TicTacToe;
+
+public class Player {
+    private Symbol symbol;
+    private Field field;
+
+    Player(){
+
+    }
+
+    Player(Symbol symbol, Field field){
+        this.symbol = symbol;
+        this.field = field;
+    }
+
+    void putSymbol(int pos){
+        this.field.setSymbol(this.symbol,pos);
+    }
+
+
+}
